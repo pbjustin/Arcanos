f6aa4a6eedae59eb29e6b607806ff649228ddeb4
pbjustin <pbjustin@gmail.com>
2025-07-15 02:59:38 -0400

    Delete src_routes_api.route_Version4.ts

--- src_routes_api.route_Version4.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import testOpenAIRouter from '../test-openai.js';
-
-const router = express.Router();
-
-router.use('/ask', testOpenAIRouter);
-
-export default router;
