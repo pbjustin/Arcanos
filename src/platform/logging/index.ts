a5c5120f99d616c70e2e5f5088c5305b50cfac16
User <you@example.com>
2026-02-25 01:57:27 -0500

    chore(logging): add platform logging module exports

---
+++ src/platform/logging/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Logging System Entry Point
+ */
+
+export * from "./logger.js";
+export * from "./specialized.js";
+export * from "./telemetry.js";
+export * from "./unifiedTelemetry.js";
+export * from "./diagnostics.js";
+export * from "./dualModeAudit.js";
+export * from "./systemDiagnostics.js";
+export { default } from "./logger.js";
